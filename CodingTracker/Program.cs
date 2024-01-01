dbc2ee33fa610aae770b6c2a6e482d0a8beacfd5
martinka-h <132383732+martinka-h@users.noreply.github.com>
2024-01-01 17:43:21 +0200

    creation of sqlite database and table

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Data.Sqlite;
+
+class Program
+{
+    static string connectionString = @"Data Source = codingtracker.db";
+
+    static void Main(string[] args)
+    {
+        using (var connection = new SqliteConnection(connectionString))
+        {
+            connection.Open();
+            var tableCmd = connection.CreateCommand();
+
+            tableCmd.CommandText =
+                @"CREATE TABLE IF NOT EXISTS coding_sessions (
+                Id INTEGER PRIMARY KEY,
+                StartDateTime TEXT,
+                EndDateTime TEXT
+                Duration REAL)";
+
+            tableCmd.ExecuteNonQuery();
+            connection.Close();
+        }
+    }
+
+}
