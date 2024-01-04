9b355068b56062330c9a4692b9701209b0aebd67
martinka-h <132383732+martinka-h@users.noreply.github.com>
2024-01-04 12:05:22 +0200

    UpdateRecord

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -2,6 +2,7 @@
 using CodingTracker;
 using ConsoleTableExt;
 using System.Security.Cryptography;
+using System.Transactions;
 
 class Program
 {
@@ -94,7 +95,59 @@ Choose one of the following options:
     }
     private static void UpdateRecord()
     {
-        throw new NotImplementedException();
+        Console.Clear();
+        ViewAllRecords();
+
+        string recordId = Helpers.GetNumperInput("\nProvide the id of the record you want to update.");
+
+        using (var connection = new SqliteConnection(connectionString))
+        {
+            connection.Open();
+            var checkCmd = connection.CreateCommand();
+            checkCmd.CommandText = $"SELECT EXISTS(SELECT 1 FROM coding_sessions WHERE Id = {recordId})";
+            int check = Convert.ToInt32(checkCmd.ExecuteScalar());
+
+            if (check == 0)
+            {
+                Console.WriteLine($"Record with Id {recordId} does not exist." +
+                    $"\n Type 0 to go back to main menu, or any other key to update another record.");
+                connection.Close();
+
+                if (Console.ReadKey().Key == ConsoleKey.D0)
+                {
+                    Console.Clear();
+                    MainMenu();
+                }
+                else
+                {
+                    UpdateRecord();
+                }
+            }
+
+            string newDate = "";
+            var tableCmd = connection.CreateCommand();
+
+            Console.WriteLine("\nType 1 to update the start date, or type 2 to update the end date.");
+
+            if (Console.ReadLine() == "1")
+            {
+                newDate = Helpers.GetDateTimeInput("Please provide a new start date");
+                tableCmd.CommandText = $"UPDATE coding_sessions SET startDateTime = '{newDate}'";
+                
+            }
+            else if (Console.ReadLine() == "2")
+            {
+                newDate = Helpers.GetDateTimeInput("Please probide a new end date");
+                tableCmd.CommandText = $"UPDATE coding_sessions SET endDateTime = '{newDate}'";
+            }
+
+            tableCmd.ExecuteNonQuery();
+            connection.Close();
+
+            Console.WriteLine($"The record with Id {recordId} was updated. Press any key to continue");
+            Console.ReadLine();
+            MainMenu();
+        }
     }
     private static void DeleteRecord()
     {
@@ -121,7 +174,7 @@ Choose one of the following options:
                 DeleteRecord();
             }
 
-            Console.WriteLine($"\nRecord with Id {recordId} was deleted."); 
+            Console.WriteLine($"\nRecord with Id {recordId} was deleted.");
 
             if (rowCount > 0)
             {
@@ -129,7 +182,8 @@ Choose one of the following options:
 
                 if (Console.ReadLine() == "0") MainMenu();
                 else DeleteRecord();
-            } else if (rowCount < 1)
+            }
+            else if (rowCount < 1)
             {
                 Console.WriteLine("Press Enter to continue to main menu");
                 Console.ReadKey();
